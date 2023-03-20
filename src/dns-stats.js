24b883d8c719db246fbc8acd3bbec3130155107d
VitaliMay <mvt30@mail.ru>
2023-03-20 18:41:16 +0300

    dns-stats Solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
+function getDNSStats(domains) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  // сформировал строку из исходного массива
+  let arrStr = domains.join('.')
+
+  let result = {}
+  // делаю из каждой строки входного массива подмассив и переворачиваю его
+  for (key in domains) domains[key] = domains[key].split('.').reverse();
+  // определяю уникальные элементы и формирую из них массив
+  let uniq = [...new Set(domains.flat())]
+
+  // если массив уникальных элементов пустой, сворачиваем функцию
+  if(uniq.length === 0) return result;
+
+  result['.'+ uniq[0]] = arrStr.split(uniq[0]).length - 1
+  result['.'+ uniq[0] + '.' + uniq[1]] = arrStr.split(uniq[1] + '.' + uniq[0]).length - 1
+
+  if (uniq.length > 2) {
+    let nameKey = '.'+ uniq[0] + '.' + uniq[1]
+    console.log('name = ' + nameKey)
+    for (let i = 2; i < uniq.length; i++) {
+        result[nameKey + '.' + uniq[i]] = arrStr.split(uniq[i] + '.' + uniq[1] + '.' + uniq[0]).length - 1
+    }
+  }
+  return result
 }
 
 module.exports = {
