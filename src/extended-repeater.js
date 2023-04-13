f79a9e741aa1165949ace643cbdca454e2dfbc58
VitaliMay <mvt30@mail.ru>
2023-04-13 13:18:44 +0300

    extended-repeater Solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let arr
+  let result = ''
+  let parameters = {repeatTimes: 1, separator: '+', addition: '', additionRepeatTimes: 1, additionSeparator: '|'}
+  // переписываю свойства в parfmeters в соответствии с приходящими в options, если они есть
+  for (key in options) parameters[key] = options[key];
+  if (parameters.addition === null) parameters.addition = 'null'  /* проверка на null для одного хитровыдуманного теста */
+  arr = new Array(parameters.additionRepeatTimes) /* создаю пустой массив нужного размера */
+  arr = arr.fill(parameters.addition) /* заполняю массив */
+  result = str + arr.join(parameters.additionSeparator) /* формирую строку, которую надо повторять */
+    /* для итогового результата фактически повторяю предыдущие действия */
+  return Array(parameters.repeatTimes).fill(result).join(parameters.separator)
 }
 
 module.exports = {
