c98cee41519d86c6a0fe0d2cd4bd4f8766ad0949
VitaliMay <mvt30@mail.ru>
2023-04-14 11:16:23 +0300

    mine-sweeper Solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let result = [] // объявляю результирующий массив
+
+  for (let i = 0; i < matrix.length; i++) {
+    result[i] = [] // продолжаю формировать массив результата
+    for (let k = 0; k < matrix[i].length; k++) {
+      result[i][k] = 0 // присваиваю начальные значерия в массив результата
+      // суть решения - обхожу по кругу каждую ячейку матрицы,
+      // если нахожу мину(true) увеличиваю значение счётчика (или соответствующей ячейки результирующего массива)
+      if ((i-1) >= 0 && (k-1) >= 0 && matrix[i-1][k-1] === true) result[i][k] = result[i][k] + 1;
+      if ((i-1) >= 0 && (k) >= 0 && matrix[i-1][k] === true) result[i][k] = result[i][k] + 1;
+      if ((i-1) >= 0 && (k + 1) < matrix[i].length && matrix[i-1][k+1] === true) result[i][k] = result[i][k] + 1;
+      if ((i) >= 0 && (k + 1) < matrix[i].length && matrix[i][k+1] === true) result[i][k] = result[i][k] + 1;
+      if ((i+1) < matrix.length && (k + 1) < matrix[i].length && matrix[i+1][k+1] === true) result[i][k] = result[i][k] + 1;
+      if ((i+1) < matrix.length && (k) < matrix[i].length && matrix[i+1][k] === true) result[i][k] = result[i][k] + 1;
+      if ((i+1) < matrix.length && (k-1) >= 0 && matrix[i+1][k-1] === true) result[i][k] = result[i][k] + 1;
+      if ((i) < matrix.length && (k-1) >= 0 && matrix[i][k-1] === true) result[i][k] = result[i][k] + 1;
+    }
+  }
+  return result
 }
 
 module.exports = {
