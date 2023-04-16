0a2fa403064969b7e2d7284cc1d6cc9ab05f2634
VitaliMay <mvt30@mail.ru>
2023-04-16 21:10:33 +0300

    simple-chain Solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  
+  result: '',
+  finish: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.result.length === 0) return 0;
+    return this.result.split('~~').length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+    if (this.getLength() === 0) {this.result = this.result + `( ${value} )`}
+    else {this.result = this.result + `~~( ${value} )`}
+    return this
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    this.finish = this.result.split('~~')
+    // со всеми этими проверками тест проходит)
+    //if (this.result.length === 0 || typeof position !== 'number' || !Number.isInteger(position) || position <= 0 || position > this.getLength()) {
+    //if (typeof position !== 'number' || !Number.isInteger(position) || position <= 0 || position > this.getLength()) {
+    if (!Number.isInteger(position) || position <= 0 || position > this.getLength()) {
+      this.finish = [] // надо обнулять, иначе не проходит тест
+      this.result = '' // надо обнулять, иначе не проходит тест
+      throw new Error ("You can't remove incorrect link!")
+    } else {
+      this.finish.splice(position-1, 1)
+      this.result = this.finish.join('~~')
+    }
+
+    return this
+
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.result = this.result.split('~~').reverse().join('~~')
+    return this
+
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.finish = [this.result]
+    this.result = ''
+    return this.finish.pop()
   }
 };
 
